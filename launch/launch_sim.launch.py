04a64983dd9ec84f336800db37791407d44ebf4d
MJR Robotics <danilomodestojr@gmail.com>
2024-07-22 17:09:45 +0800

    Completed Driving Sim module

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,46 +1,43 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
-
-
 def generate_launch_description():
+    package_name = 'my_bot'  # Your package name
 
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='my_bot' #<--- CHANGE ME
-
-    rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    # Declare the 'world' argument
+    declare_world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=os.path.join(get_package_share_directory(package_name), 'worlds', 'empty.world'),
+        description='Specify world file name'
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+    # Get the value of the 'world' argument
+    world = LaunchConfiguration('world')
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py'
+        )]), launch_arguments={'world': world}.items()
+    )
+
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
+                        arguments=['-topic', 'robot_description', '-entity', 'my_bot'],
                         output='screen')
 
-
-
-    # Launch them all!
     return LaunchDescription([
+        declare_world_arg,
         rsp,
         gazebo,
         spawn_entity,
